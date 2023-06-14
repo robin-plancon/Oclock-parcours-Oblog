48333be92dfc335273d369c3ffdf6ee83802c908
robin-plancon <robin.plancon@outlook.com>
2023-06-14 09:48:23 +0200

    use json data to display article in index

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const articles = require('./data/articles.json');
 
 const app = express();
 const port = 5050;
 
+// configuration to use ejs
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('static'));
+
+// set my articles data to use it globally in my views
+app.locals.myArticles = articles;
+
+// default route that render index page
 app.get('/', (req, res) => {
-	res.sendFile(`${__dirname}/integration/index.html`);
+	res.render('index');
 });
 
 app.listen(port, () => {
